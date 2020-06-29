65f221f5a783c8aa8664eceafd4e566938a3e79a
saruj <msaruj@.com>
2020-06-29 21:50:46 +0600

    Updates

---
+++ precache-manifest.20958882025c77f41a733c899b03af68.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9438620d883516ffa287263aeb11bda5",
+    "url": "/react-myportfolio/index.html"
+  },
+  {
+    "revision": "7d6ae0f7c51279b355e1",
+    "url": "/react-myportfolio/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "9c4e7137d4e904a9b69b",
+    "url": "/react-myportfolio/static/css/main.b80784cd.chunk.css"
+  },
+  {
+    "revision": "7d6ae0f7c51279b355e1",
+    "url": "/react-myportfolio/static/js/2.0d500d0f.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/react-myportfolio/static/js/2.0d500d0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c4e7137d4e904a9b69b",
+    "url": "/react-myportfolio/static/js/main.38217c16.chunk.js"
+  },
+  {
+    "revision": "f29651ddd24f380063cb",
+    "url": "/react-myportfolio/static/js/runtime-main.38caa9ce.js"
+  },
+  {
+    "revision": "67f59f16e0d000c3fd014a3ee85d4e43",
+    "url": "/react-myportfolio/static/media/Saruj.67f59f16.jpg"
+  }
+]);
